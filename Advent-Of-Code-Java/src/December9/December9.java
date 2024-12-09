8cf24e0871b9c11b277785ef903e1b46e617e65c
stoftot <gryn@itu.dk>
2024-12-09 18:25:29 +0100

    Solution for day 9

--- Advent-Of-Code-Java/src/December9/December9.java
+++ Advent-Of-Code-Java/src/December9/December9.java
@@ -1,11 +1,13 @@
 package December9;
 
 import javax.management.ValueExp;
+import java.awt.*;
 import java.io.File;
 import java.util.*;
 
 //Traps remember to keep the ids grouped and not as a single char
 //this was a problem when creating one string and swapping the characters
+//only look to the left of it
 public class December9 {
     public static void main(String[] args) {
         try {
@@ -61,26 +63,34 @@ public class December9 {
     }
 
     public static void Second() throws Exception {
-        var input = new File("src/December9/Data/test.txt");
+        var input = new File("src/December9/Data/input.txt");
         var myReader = new Scanner(input);
         var data = myReader.nextLine().split("");
         var disk = new ArrayList<String>();
         var id = 0;
         var freeSpaceMap = new HashMap<Integer, Integer>();
-        var idMap = new HashMap<Integer, Integer>();
+        var idMap = new HashMap<Integer, DiskData>();
 
         for (var i = 0; i < data.length; i++) {
+            int value = Integer.parseInt(data[i]);
             if (i % 2 == 0) {
-                for (var j = 0; j < Integer.parseInt(data[i]); j++) {
+                for (var j = 0; j < value; j++) {
                     disk.add(String.valueOf(id));
                 }
-                idMap.put(id, Integer.parseInt(data[i]));
+                
+                idMap.put(id, new DiskData(){{
+                    size = value;
+                    lastIndexOfBlock = disk.size()-1;
+                }});
+                
                 id++;
             } else {
-                for (var j = 0; j < Integer.parseInt(data[i]); j++) {
+                for (var j = 0; j < value; j++) {
                     disk.add(".");
                 }
-                freeSpaceMap.put(i, Integer.parseInt(data[i]));
+                if (value > 0) {
+                    freeSpaceMap.put(disk.size()-value, value);
+                }
             }
         }
 
@@ -88,14 +98,45 @@ public class December9 {
         while (disk.get(pointer).equals(".")) {
             pointer--;
         }
-        ArrayList<Integer> freeSpaceKeys = new ArrayList<>(freeSpaceMap.keySet());
-        Collections.sort(freeSpaceKeys);
-        
-        for (int i = id; id >= 0; i--) {
-            int spaceNeeded = idMap.get(i);
+
+        for (int i = id - 1; i >= 0; i--) {
+            int spaceNeeded = idMap.get(i).size;
             //Find free space
-            for(int freeSpaceKey : freeSpaceKeys) {
+            ArrayList<Integer> freeSpaceKeys = new ArrayList<>(freeSpaceMap.keySet());
+            Collections.sort(freeSpaceKeys);
+            for (int freeSpaceKey : freeSpaceKeys) {
+                int freeSpace = freeSpaceMap.get(freeSpaceKey);
+
+                //if it cant find space on the left of it, then break
+                if(freeSpaceKey > idMap.get(i).lastIndexOfBlock){
+                    break;
+                }
+                
+                
+                if (freeSpace >= spaceNeeded) {
+                    //Move block
+                    pointer = idMap.get(i).lastIndexOfBlock;
+                    if(!tryToMoveBlock(freeSpaceKey, freeSpaceKey + (spaceNeeded-1), pointer - (spaceNeeded-1), pointer, disk)) {
+                        System.out.println("Failed to move block");
+                    }
+                    
+                    //Update free space
+                    freeSpaceMap.remove(freeSpaceKey);
+                    if (freeSpace != spaceNeeded) {
+                        freeSpaceMap.put(freeSpaceKey + spaceNeeded, freeSpace - spaceNeeded);
+                    }
+                    break;
+                }
             }
+//            pointer -= spaceNeeded;
+//            while (disk.get(pointer).equals(".")) {
+//                pointer--;
+//            }
+//            System.out.println("id: " + i);
+//            for (var u = 0; u < disk.size(); u++) {
+//                System.out.print(disk.get(u));
+//            }
+//            System.out.println();
         }
 
 //        for (var i = 0; i < pointer; i++) {
@@ -127,11 +168,12 @@ public class December9 {
 //                i = second;
 //            }
 //        }
-        
+
         //print disk
-        for (var i = 0; i < disk.size(); i++) {
-            System.out.print(disk.get(i));
-        }
+//        for (var i = 0; i < disk.size(); i++) {
+//            System.out.print(disk.get(i));
+//        }
+//        System.out.println();
 
         long sum = 0;
 
@@ -142,6 +184,11 @@ public class December9 {
         }
 
         System.out.println(sum);
+    }
+    
+    private static class DiskData{
+        public int size;
+        public int lastIndexOfBlock;
     }
 
     private static void swapCharacter(int first, int second, ArrayList<String> list) {
@@ -156,11 +203,11 @@ public class December9 {
 //                return false;
 //            }
 //        }
-        if(second-first < fourth-third){
+        if (second - first < fourth - third) {
             return false;
         }
 
-        for (int i = first; i < second; i++) {
+        for (int i = first; i <= second; i++) {
             swapCharacter(i, third, list);
             third++;
         }
